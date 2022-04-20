e16cab9dfa80a2acf9182660511dfd77315ad23b
Fabio Sorrentino <fabio180896@icloud.com>
2022-04-20 15:37:16 +0200

    remove component prova

--- fright_n_fight/src/app/app.module.ts
+++ fright_n_fight/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProvaComponent } from './prova/prova.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProvaComponent
   ],
   imports: [
     BrowserModule
