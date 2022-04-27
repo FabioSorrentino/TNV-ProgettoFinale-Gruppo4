727f4ee214b2a752b297174c0c5d0abf8bc06776
Fabio Sorrentino <fabio180896@icloud.com>
2022-04-27 17:29:16 +0200

    aggiunta servizi user ai componenti

--- fright_n_fight/FE_Angular/src/app/components/insert-rating/insert-rating.component.ts
+++ fright_n_fight/FE_Angular/src/app/components/insert-rating/insert-rating.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { BackendApiService } from 'src/app/service/backend-api.service';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
+import { MovieApiService } from 'src/app/service/movie-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Rating } from 'src/app/models/rating';
 
 
 @Component({
@@ -11,13 +15,20 @@ import { BackendApiService } from 'src/app/service/backend-api.service';
 })
 export class InsertRatingComponent implements OnInit {
 
-  constructor(private httpClient : HttpClient, private backendAPIService: BackendApiService) { }
+  movie_id: number = 0;
+
+  constructor(private httpClient : HttpClient, private backendAPIService: BackendApiService, public tokenStorageService: TokenStorageService,
+    activatedRoute: ActivatedRoute, public movieAPIService: MovieApiService) { 
+      this.movie_id = +activatedRoute.snapshot.params['movie_id'];
+    }
 
   ngOnInit(): void {
   }
 
   createNewRating (ratingForm: NgForm) {
-    this.backendAPIService.createNewRating(ratingForm.value).subscribe({
+    let userId: number = this.tokenStorageService.getUserId();
+    let firstRating: Partial<Rating> ={user_id: userId, movie_id: this.movie_id, movie_rating: ratingForm.value.movie_rating};
+    this.backendAPIService.createNewRating(firstRating).subscribe({
       next: () => console.log('New rating created!'),
       error: () => console.log('Error!')
     });
