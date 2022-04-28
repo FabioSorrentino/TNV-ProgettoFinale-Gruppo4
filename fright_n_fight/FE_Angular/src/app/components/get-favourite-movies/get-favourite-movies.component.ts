ffb14fa350d368b7f02e1950ec9c432eb11184b2
Fabio Sorrentino <fabio180896@icloud.com>
2022-04-28 12:34:13 +0200

    aggiunta html al componente listaFav

--- fright_n_fight/FE_Angular/src/app/components/get-favourite-movies/get-favourite-movies.component.ts
+++ fright_n_fight/FE_Angular/src/app/components/get-favourite-movies/get-favourite-movies.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FavouriteMovie } from 'src/app/models/favouriteMovie';
+import { MovieCredits } from 'src/app/models/movieCredits';
+import { MovieData } from 'src/app/models/movieData';
 import { BackendApiService } from 'src/app/service/backend-api.service';
+import { MovieApiService } from 'src/app/service/movie-api.service';
 import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 
@@ -14,21 +17,31 @@ export class GetFavouriteMoviesComponent implements OnInit {
 
   userId: number | null = null;
   movies: FavouriteMovie[] = [];
+  moviesData: MovieData []= [];
+  moviesCredits: MovieCredits [] = [];
 
-  constructor(private backendAPIService: BackendApiService, private activatedRoute: ActivatedRoute, public tokenStorageService: TokenStorageService) {
+  constructor(private backendAPIService: BackendApiService, private activatedRoute: ActivatedRoute,
+    public tokenStorageService: TokenStorageService, public movieAPIService: MovieApiService) {
    }
 
   ngOnInit(): void {
+    this.userId = this.tokenStorageService.getUserId();
     this.getAllFavouriteMovies();
   }
 
   getAllFavouriteMovies(){
-    this.userId = this.tokenStorageService.getUserId();
-    this.backendAPIService.getAllFavouriteMoviesByUser(this.userId).subscribe({
-      next: (res) => this.movies = res,
-      error: () => console.log('Error!'),
-      complete: () => console.log('Complete')
-  });
+    this.backendAPIService.getAllFavouriteMoviesByUserId(this.userId).subscribe({
+      next: (res) => { this.movies = res;
+        for (let i = 0; i < this.movies.length; i++) {
+          let movieId = this.movies[i].movie_id;
+          this.movieAPIService.getMovieCredits(movieId).subscribe({
+            next : (val) => this.moviesCredits[i]= val
+          }),
+          this.movieAPIService.getMovieDetails(movieId).subscribe({
+            next : (resu) => this.moviesData[i] = resu
+          })
+        }
+      }
+    })
   }
-
 }
