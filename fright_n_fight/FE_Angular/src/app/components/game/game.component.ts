466c63017ba8c47e44d6d729841bc10cfb93b7da
Davide Saddi <davide.saddi@outlook.com>
2022-04-30 17:23:36 +0200

    x

--- fright_n_fight/FE_Angular/src/app/components/game/game.component.ts
+++ fright_n_fight/FE_Angular/src/app/components/game/game.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { interval } from 'rxjs';
+import { interval, Subscriber } from 'rxjs';
 
 import { MovieCredits } from 'src/app/models/movieCredits';
 import { MovieData, Genre } from 'src/app/models/movieData';
@@ -15,10 +15,6 @@ import { MovieApiService } from 'src/app/service/movie-api.service';
 })
 export class GameComponent implements OnInit {
 
-  /*Booleani di start e di fine gioco*/
-  start: boolean = false;
-  finish: boolean = false;
-
   /*Mostrare o meno info film e aggiungere del tempo*/
   showPopularity : boolean = false;
   showBudget : boolean = false;
@@ -26,7 +22,7 @@ export class GameComponent implements OnInit {
   showVote : boolean = false;
   showRuntime : boolean = false;
 
-    /*Variabili per contatore*/
+  /*Variabili per contatore*/
   subscribeTimer: number = 0;
   timeAdded : number = 0;
   seconds : string | null = null;
@@ -42,60 +38,52 @@ export class GameComponent implements OnInit {
   movieCredits: MovieCredits| null = null;
   genres: Genre[] = [];
 
+  /*Booleani di start e di fine gioco*/
+  start: boolean = false;
+  finish: boolean = false;
 
-  //Punteggio
-  points: number | null = null;
+  /*Gestione punteggio giocatore*/
+  points: number = 0;
+  ptRemoved: number = 0;
 
-  
   constructor(
     public newMovieService: MovieApiService,
-    private router:Router
+    private router: Router
     ){ }
 
   ngOnInit(): void {
-  }
-
-
+    }
 
   onStart(){
     this.start = true;
     this.getMovie();
     this.timer();
-   
     }
-
 
     onClickShowRuntime() {
       this.showRuntime = true;
       this.timeAdded = this.timeAdded + 30;
     }
 
-    
     onClickShowBudget(){
       this.showBudget = true;
       this.timeAdded = this.timeAdded + 30;
-
     }
 
     onClickShowVote(){
       this.showVote = true;
       this.timeAdded = this.timeAdded + 30;
-
     }
 
     onclickOrLanguage(){
-
-          //window.alert('No description!');
+      //window.alert('No description!');
       this.showOrLanguage = true;
       this.timeAdded = this.timeAdded + 30;
-      
     }
 
     onClickShowPopularity(){
-
       this.showPopularity = true;
       this.timeAdded = this.timeAdded + 30;
-     
     }
 
     onStop() {
@@ -110,46 +98,56 @@ export class GameComponent implements OnInit {
         const source = interval(1000);
         
         const abc = source.subscribe(val => {  
-        if(this.finish) return
-        this.subscribeTimer = this.timeAdded + val,         
-        this.minutes = Math.floor(this.subscribeTimer % 3600 / 60).toString().padStart(2,'0'),
-        this.seconds = Math.floor(this.subscribeTimer % 60).toString().padStart(2,'0')
+
+          if(val === 25) this.ptRemoved = this.ptRemoved - 20;
+          if(val === 45) this.ptRemoved = this.ptRemoved - 20;
+          if(val === 55) this.ptRemoved = this.ptRemoved - 20;
+          if(val === 75) this.ptRemoved = this.ptRemoved - 20;
+          if(val === 85) this.ptRemoved = this.ptRemoved - 20;
+
+          if(this.finish) return
+          this.subscribeTimer = this.timeAdded + val,         
+          this.minutes = Math.floor(this.subscribeTimer % 3600 / 60).toString().padStart(2,'0'),
+          this.seconds = Math.floor(this.subscribeTimer % 60).toString().padStart(2,'0')
       });
     }
-  
-
-    getMovie(){
-        this.movieId = this.getRandomInt(this.maxRandom);
-        console.log(this.movieId);
     
-        this.newMovieService.getMovieDetails(this.movieId).subscribe({
-          next: (res)=> {
-              this.movieSpecs = res;
-              if(this.movieSpecs === null || this.movieSpecs.poster_path === null || this.movieSpecs.tagline === null || this.movieCredits === null || this.movieCredits?.cast === null || this.movieCredits?.crew === null) 
-              this.getMovie();
-          },  
-          error: ()=> {
-              this.getMovie(); 
-        },
-        });
-
-    this.newMovieService.getMovieCredits(this.movieId).subscribe({
-      next:(res)=>{
-            this.movieCredits = res;
-      }
-      });
-}
-  
-
     verifyResult(verifyForm: NgForm){
         if(this.movieSpecs?.title.toLowerCase()===verifyForm.value.Title.toLowerCase()){
           this.finish = true;
           this.points = this.subscribeTimer;
+          this.points = this.points + this.ptRemoved;
           console.log("Hai indovinato!");
         }
         else {
-      console.log("Ops...hai sbagliato :(");
+          console.log("Ops...hai sbagliato :(");
         }
-}
+    }
 
+    getMovie(){
+      this.movieId = this.getRandomInt(this.maxRandom);
+      console.log(this.movieId);
+  
+      this.newMovieService.getMovieDetails(this.movieId).subscribe({
+        next: (res)=> {
+            this.movieSpecs = res;
+            if(this.movieSpecs === null || 
+              this.movieSpecs.poster_path === null ||
+              this.movieSpecs.tagline === null || 
+              this.movieCredits === null || 
+              this.movieCredits?.cast === null || 
+              this.movieCredits?.crew === null) 
+            
+                this.getMovie();
+                    },  
+        
+        error: ()=> {
+            this.getMovie(); 
+        },
+    });
+
+  this.newMovieService.getMovieCredits(this.movieId).subscribe({
+    next:(res)=>{ this.movieCredits = res;}
+    });
+  }
 }
